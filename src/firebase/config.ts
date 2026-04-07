db3598e4b28c1a8489329e261b2c34c87a95fd14
Baraka Ruzibiza <barakaruzibiza680@gmail.com>
2026-04-07 21:35:19 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5852085552-383c3",
+  "appId": "1:17312908574:web:17346dcf91641ec75c750f",
+  "apiKey": "AIzaSyCAoz38A1YjyICA7VbHXRVVJMLTtNNizKA",
+  "authDomain": "studio-5852085552-383c3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "17312908574"
+};
